a506dbef1052ab9f218526b6bb8db66ba233146c
Ivelin Ivanov <5932950+ivelin-links@users.noreply.github.com>
2023-07-26 19:24:39 +0300

    Initial commit

---
+++ scripts/helpmenu.js
@@ -0,0 +1,6 @@
+stm_bm(["menu1",430,"","blank.gif",0,"","",0,0,0,0,50,1,0,0,"","",0],this);
+stm_bp("p0",[0,4,0,0,0,0,0,0,100,"",-2,"",-2,90,0,0,"#7f7f7f","#000000","",3,0,0,"#000000"]);
+stm_ai("p0i0",[2,"","images/help.gif","images/helpsel.gif",87,22,0,"help.htm","_self","","","","",0,0,0,"","",0,0,0,1,1,"#ffffff",1,"#ffffff",1,"","",3,3,0,0,"#ffffff","#ffffff","#ffffff","#ffcc00","bold 8pt Arial","bold 9pt Arial",0,0]);
+stm_aix("p0i1","p0i0",[2,"","images/about.gif","images/aboutsel.gif",87,22,0,"javascript:open(\'about.htm\',\'about\',config=\'width=350,height=300\');","_self","","","","",0,0,0,"","",0,0,0,1,1,"#ffffff",1,"#ffffff",1,"","",3,3,0,0,"#ffffff","#ffffff","#ffffff","#ffcc00","bold 9pt Arial"]);
+stm_ep();
+stm_em();
